0a5f5777a987d5cecf4c036cce397c30e5aa9e0e
Edyta <edysiaborowska27@gmail.com>
2024-03-09 20:06:37 +0100

    first test for reader module

--- src/test/java/org/example/readerarticleretriever/ReaderArticleRetrieverFacedeTest.java
+++ src/test/java/org/example/readerarticleretriever/ReaderArticleRetrieverFacedeTest.java
@@ -1,5 +1,6 @@
 package org.example.readerarticleretriever;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,13 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class ReaderArticleRetrieverFacedeTest {
 
     @Test
-    public void f(){
+    @DisplayName("should return article with id 1 and content xxx")
+    public void should_return_article_with_id_1_and_content_xxx(){
         //given
         ReaderArticleRetrieverFacede facede = new ReaderArticleRetrieverFacede();
         //when
         //ArticleDto articleDto = facede.retrieveById(1);
         ArticleDto articleDto = facede.retrieveById(1L);
         //then
+        assert articleDto.id().equals(1L);
+        assert articleDto.content().equals("5 kroków: 1, 2, 3, 4");
+        assert articleDto.header().equals("jak zostać programistą");
+        assert articleDto.category().equals("IT");
 
 
     }
